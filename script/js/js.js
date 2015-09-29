8a694d48d4e0220fd904d8104ea034af36d65414
franklin lugo <franklinlugo2805@gmail.com>
2015-09-29 11:33:32 -0430

    fixed btn partes and orientacione

--- script/js/js.js
+++ script/js/js.js
@@ -1,13 +1,33 @@
 $(document).ready(function(){
 
+function cerrarPartes() {
+	$("#partes--msg").hide();
+	$("#partes--btn--teclado").hide();
+	$("#partes--btn--monitor").hide();
+	$("#partes--btn--raton").hide();
+	$("#partes--btn--caja").hide();
+	$("#orientaciones--msg").hide();
+	$("#orientaciones--btn1").hide();
+	$("#orientaciones--btn2").hide();
+	$("#orientaciones--btn3").hide();
+
+}
+function cerrarOrientaciones(){
+	$("#orientaciones--msg").hide();
+	$("#orientaciones--btn1").hide();
+	$("#orientaciones--btn2").hide();
+	$("#orientaciones--btn3").hide();
+}
 //bombillo 
 $("#bombillo").click(function(){
 	$("#bombillo--msg").toggle("fast");
+	cerrarPartes();
 })
 
 // boton Computador
 $("#computador--btn").mouseenter(function(){
 	$("#computador--msg").fadeIn(1000);
+	cerrarPartes();
 })
 $("#computador--btn").mouseleave(function(){
 	$("#computador--msg").fadeOut("fast");
@@ -15,47 +35,54 @@ $("#computador--btn").mouseleave(function(){
 
 // Boton partes
 $("#partes--btn").mouseenter(function(){
-	$("#partes--msg").fadeIn(1000);
-})
-$("#partes--btn").mouseleave(function(){
-	$("#partes--msg").fadeOut(1000);
+	$("#partes--msg").show();
+	var foo = $("#partes--msg").css("display");
+	// alert(foo);
+	if (foo=="block"){
+		$("#partes--btn--teclado").show();
+		$("#partes--btn--monitor").show();
+		$("#partes--btn--raton").show();
+		$("#partes--btn--caja").show();
+	}
+	cerrarOrientaciones();
 })
 
 // teclado
 $("#partes--btn--teclado").mouseenter(function(){
-	$("#partes--msg--teclado").fadeIn();
+	$("#partes--msg--teclado").show();
 })
 $("#partes--btn--teclado").mouseleave(function(){
-	$("#partes--msg--teclado").fadeOut();
+	$("#partes--msg--teclado").hide();
 })
 
 // Monitor
 $("#partes--btn--monitor").mouseenter(function(){
-	$("#partes--msg--monitor").fadeIn();
+	$("#partes--msg--monitor").show();
 })
 $("#partes--btn--monitor").mouseleave(function(){
-	$("#partes--msg--monitor").fadeOut();
+	$("#partes--msg--monitor").hide();
 })
 
 // Raton
 $("#partes--btn--raton").mouseenter(function(){
-	$("#partes--msg--raton").fadeIn();
+	$("#partes--msg--raton").show();
 })
 $("#partes--btn--raton").mouseleave(function(){
-	$("#partes--msg--raton").fadeOut();
+	$("#partes--msg--raton").hide();
 })
 
 // Caja
 $("#partes--btn--caja").mouseenter(function(){
-	$("#partes--msg--caja").fadeIn();
+	$("#partes--msg--caja").show();
 })
 $("#partes--btn--caja").mouseleave(function(){
-	$("#partes--msg--caja").fadeOut();
+	$("#partes--msg--caja").hide();
 })
 
 // Uso
 $("#uso--btn").mouseenter(function(){
 	$("#uso--msg").fadeIn(1000);
+	cerrarPartes();
 })
 $("#uso--btn").mouseleave(function(){
 	$("#uso--msg").fadeOut("fast");
@@ -64,6 +91,7 @@ $("#uso--btn").mouseleave(function(){
 // Aplicaciones
 $("#aplicaciones--btn").mouseenter(function(){
 	$("#aplicaciones--msg").fadeIn(1000);
+	cerrarPartes();
 })
 $("#aplicaciones--btn").mouseleave(function(){
 	$("#aplicaciones--msg").fadeOut("fast");
@@ -72,6 +100,7 @@ $("#aplicaciones--btn").mouseleave(function(){
 // Internet
 $("#internet--btn").mouseenter(function(){
 	$("#internet--msg").fadeIn(1000);
+	cerrarPartes();
 })
 $("#internet--btn").mouseleave(function(){
 	$("#internet--msg").fadeOut("fast");
@@ -80,9 +109,13 @@ $("#internet--btn").mouseleave(function(){
 // orientaciones
 $("#orientaciones--btn").mouseenter(function(){
 	$("#orientaciones--msg").fadeIn(1000);
-})
-$("#orientaciones--btn").mouseleave(function(){
-	$("#orientaciones--msg").fadeOut("fast");
+	var foo = $("#orientaciones--msg").css("display");
+	// alert(foo);
+	if (foo=="block"){
+		$("#orientaciones--btn1").show();
+		$("#orientaciones--btn2").show();
+		$("#orientaciones--btn3").show();
+	}
 })
 
 // Orientaciones
